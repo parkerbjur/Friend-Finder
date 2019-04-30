30d3c5745a2d883d65c72ebd9a120554fd2a3288
Tyoff45 <bjur.parker45@gmail.com>
2019-04-29 17:00:56 -0700

    commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Friend-Finder
+
+![Example Video](https://photos.app.goo.gl/6QuewZ27uVpNS51A8)
--- server.js/server.js
+++ server.js/server.js
@@ -8,9 +8,18 @@ const PORT = 8080;
 
 //global variable
 let friends = [
-    {name: "parker",
-     scores: [1, 2, 3, 4, 5],
+    {
+        name: "parker",
+        scores: [1, 2, 3, 4, 5],
     },
+    {
+        name: "all zero",
+        scores: [0,0,0,0,0]
+    },
+    {
+        name: "all 5",
+        scores: [5,5,5,5,5]
+    }
 ];
 
 //MySql
@@ -51,26 +60,40 @@ app.get("/api/friends", function(req, res){
 })
 
 app.post("/api/friends", function(req, res){
-    findFriend(req.body.scores);
+    console.log(req.body)
+    let newAnswers = [];
+    newAnswers[0] = req.body.group1
+    newAnswers[1] = req.body.group2
+    newAnswers[2] = req.body.group3
+    newAnswers[3] = req.body.group4
+    newAnswers[4] = req.body.group5
+    res.json(friends[findFriend(newAnswers)])
 })
 
 //double for loop indicates it could be done recursively (consider refactoring)
 function findFriend(newAnswers) {
-    let currentBestFriend;
+    let currentBestFriend = {
+        name: "none",
+        score: 1000,
+    };
+    console.log(friends)
     for(i = 0; i < friends.length; i++){
         let oldAnswers = friends[i].scores;
-        let differenceScore;
-        for(i = 0; i < friends[i].scores.length; i++){
-            differenceScore = differenceScore + Math.abs(oldAnswers[i] - newAnswers[i]);
+        let differenceScore = 0;
+        for(j = 0; j < friends[i].scores.length; j++){
+            console.log(friends[i])
+            differenceScore = differenceScore + Math.abs(oldAnswers[j] - newAnswers[j]);
         }
+        console.log(differenceScore, "difference score")
         if(differenceScore < currentBestFriend.score){
+            console.log(currentBestFriend)
             currentBestFriend = {
                 position: i,
                 score: differenceScore,
             };
         }
     }
-    return currentBestFriend
+    return currentBestFriend.position
 }
 
 app.listen(PORT, function(){
